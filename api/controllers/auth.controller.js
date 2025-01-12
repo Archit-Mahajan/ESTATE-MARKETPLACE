106d205a98d283814d70bed4de7ad1fc48ddf153
Archit-Mahajan <archit.maha@gmail.com>
2025-01-12 12:21:41 +0530

    Complete Signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req,res,next) =>{
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -12,4 +14,24 @@ export const signup = async (req,res,next) =>{
         next(error);
     }
        
+};
+
+export const signin = async (req,res,next) =>{
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return res.status(400).json("Invalid Username");
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if (!validPassword) return res.status(400).json("Invalid Credentials");
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password:pass,...userInfo} = validUser._doc;
+        res.cookie("access_token",token,{
+            httpOnly:true
+        }).status(200).json("Valid User");
+            
+        
+    }
+        catch(error){
+            next(error);
+        }
 };
