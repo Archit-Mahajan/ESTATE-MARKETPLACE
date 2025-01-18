613c60197771f7785f739718e908c7b58bbfc5ac
Archit-Mahajan <archit.maha@gmail.com>
2025-01-19 01:49:51 +0530

    update listing fixed

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,7 +4,6 @@ import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailur
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -125,9 +124,7 @@ export default function Profile() {
         return;
       }
       dispatch(signOutUserSuccess(data));
-    // eslint-disable-next-line no-unused-vars
     } catch (error) {
-      // eslint-disable-next-line no-undef
       dispatch(signOutUserFailure(data.message));
     }
   };
@@ -146,25 +143,6 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
-
-  const handleListingDelete = async (listingId) => {
-    try {
-      const res = await fetch(`/api/listing/delete/${listingId}`, {
-        method: 'DELETE',
-      });
-      const data = await res.json();
-      if (data.success === false) {
-        console.log(data.message);
-        return;
-      }
-      setUserListings((prev) =>
-        prev.filter((listing) => listing._id !== listingId)
-      );
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -248,7 +226,7 @@ export default function Profile() {
         {showListingsError ? 'Error showing listings' : ''}
       </p>
       {userListings &&
-        userListings.length > 0 &&(
+        userListings.length > 0 &&
         <div className="flex flex-col gap-4">
           <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
           {userListings.map((listing) => (
@@ -271,21 +249,12 @@ export default function Profile() {
               </Link>
   
               <div className='flex flex-col item-center'>
-              <button
-                  onClick={() => handleListingDelete(listing._id)}
-                  className='text-red-700 uppercase'
-                >
-                  Delete
-                </button>
-                <Link to={`/update-listing/${listing._id}`}>
-                  <button className='text-green-700 uppercase'>Edit</button>
-                </Link>
-                
+                <button className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
           ))}
-         </div>
-      )}
+        </div>}
     </div>
   );
 }
