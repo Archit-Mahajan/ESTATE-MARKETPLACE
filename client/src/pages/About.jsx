fff1c57bc19effda8d0f4b8ccabf447dc65de3c5
Archit-Mahajan <archit.maha@gmail.com>
2025-01-20 00:14:22 +0530

    about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+    <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Mahajan Estate</h1>
+    <p className='mb-4 text-slate-700'>Mahajan Estate is a trusted real estate agency specializing in providing seamless buying, selling, and rental services. Our team of expert agents is committed to helping clients navigate the real estate market with ease, ensuring their experience is both rewarding and stress-free.</p>
+    <p className='mb-4 text-slate-700'>
+      At Mahajan Estate, our mission is to make your real estate journey effortless. We offer tailored advice and unmatched support, whether you are purchasing your first home, selling a property, or looking to rent. We are here to guide you through every stage of the process with professional insights and a personalized touch.
+    </p>
+    <p className='mb-4 text-slate-700'>With a strong understanding of local trends and market dynamics, our team brings in-depth knowledge and expertise to every transaction. We aim to create long-term relationships with our clients by offering exceptional service and delivering results that exceed expectations. Let Mahajan Estate turn your real estate dreams into reality!</p>
+  </div>
   )
 }
